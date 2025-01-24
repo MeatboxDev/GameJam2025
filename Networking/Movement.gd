dd06f44d370ab6f16adc02c96832cd1a3d42419c
Kimbix <alemanhumberto06@gmail.com>
2025-01-24 17:03:22 -0400

    Joining a lobby as either good or bad works

--- Networking/Movement.gd
+++ Networking/Movement.gd
@@ -1,32 +1,21 @@
 extends CharacterBody3D
 
-const DEG_45: float = PI/4.0
+const BUBBLE: PackedScene = preload("res://Testing/bubble.tscn")
+const DEG_45: float = PI / 4.0
 const DEG_N45: float = -DEG_45
+const XZ = preload("res://Networking/xyz_to_xz.gd")
 
-@onready var stomp_area: Area3D = $StompArea
-@onready var push_area: Area3D = $PushArea
+@export var body_rotation_strength: float = 10.0
+@export var decel_speed: float = 3.0
+@export var impulse_max_speed: float = 75.0
+@export var jump_force: float = 50.0
+@export var jump_speed_boost: float = 5
+@export var max_speed: float = 20.0
+@export var p_cam_sensitivity: float = 0.003
+@export var speed: float = 1.5
+@export var gravity: float = 0.98
 
-@onready var body: Node = $Body
-@onready var head: Node = $Head
-@onready var head_mesh: Node = $Head/HeadMesh
-@onready var spring_arm: SpringArm3D = $Head/SpringArm3D
-@onready var p_cam: Camera3D = $Head/SpringArm3D/PlayerCamera
-
-@export var SPEED: float = 1.5
-@export var DECEL_SPEED: float = 3.0
-@export var MAX_SPEED: float = 20.0
-
-@export var JUMP_FORCE: float = 50.0
-
-@export var SENSITIVITY = 0.01
-
-## Lower is stronger
-@export var BODY_ROTATION_STRENGTH = 10.0
-
-func _ready() -> void:
-	name = str(get_multiplayer_authority())
-	p_cam.current = is_multiplayer_authority()
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+var is_good: bool = true
 
 var _up: bool = Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W)
 var _down: bool = Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S)
@@ -34,104 +23,189 @@ var _right: bool = Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D
 var _left: bool = Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A)
 var _movement: Vector3 = Vector3.ZERO
 
-func xyz_to_xz(vec: Vector3) -> Vector2:
-	return Vector2(vec.x, vec.z)
-func normalize_xz(vec: Vector3, magnitude: float = 1.0) -> Vector3:
-	return Vector3(vec.normalized().x * magnitude, vec.y, vec.normalized().z * magnitude)
-func move_toward_xz(vec: Vector3, to: Vector3, speed: float) -> Vector3:
-	return vec.move_toward(Vector3(to.x, vec.y, to.z), speed)
 
-func calculate_horizontal_movement() -> void:
+@onready var stomp_area: Area3D = $StompArea
+@onready var push_area: Area3D = $PushArea
+@onready var body_mesh: Node = $Body
+@onready var head_node: Node = $Head
+@onready var head_mesh: Node = $Head/HeadMesh
+@onready var spring_arm: SpringArm3D = $Head/SpringArm3D
+@onready var p_cam: Camera3D = $Head/SpringArm3D/PlayerCamera
+
+
+func _ready() -> void:
+	name = str(get_multiplayer_authority())
+	p_cam.current = is_multiplayer_authority()
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	for c: Node3D in $Model.get_children():
+		for mesh: MeshInstance3D in c.get_children():
+			var new_material:StandardMaterial3D = StandardMaterial3D.new()
+			new_material.albedo_color = "0000ff" if is_good else "ff0000"
+			mesh.material_override = new_material
+
+func _calculate_horizontal_movement() -> void:
 	_movement = Vector3.ZERO
 
-	if _up and not _down: _movement += Vector3.FORWARD * SPEED
-	if _down and not _up: _movement += Vector3.BACK * SPEED
-	if _right and not _left: _movement += Vector3.RIGHT * SPEED
-	if _left and not _right: _movement += Vector3.LEFT * SPEED
+	if _up and not _down:
+		_movement += Vector3.FORWARD * speed
+	if _down and not _up:
+		_movement += Vector3.BACK * speed
+	if _right and not _left:
+		_movement += Vector3.RIGHT * speed
+	if _left and not _right:
+		_movement += Vector3.LEFT * speed
 
 	if _movement.z < 0:
-		body.rotate_y(angle_difference(body.rotation.y, head.rotation.y)/10.0)
+		body_mesh.rotate_y(angle_difference(body_mesh.rotation.y, head_node.rotation.y) / 10.0)
 	if _movement.x:
-		body.rotate_y(angle_difference(body.rotation.y + sign(_movement.x) * DEG_45, head.rotation.y)/10.0)
+		var body_head_diff: float = angle_difference(
+			body_mesh.rotation.y + sign(_movement.x) * DEG_45, head_node.rotation.y
+		)
+		body_mesh.rotate_y(body_head_diff / 10.0)
 
 	# If velocity is over 3 times the max in the XZ axys, just lock it to that
-	if xyz_to_xz(velocity).length() > MAX_SPEED * 3:
-		velocity = normalize_xz(velocity, MAX_SPEED*3)
+	if XZ.xz_length(velocity) > impulse_max_speed:
+		velocity = XZ.normalize_xz(velocity, impulse_max_speed)
+		# TODO: Add a little bit of strafing here
 		return
 
 	# First we check to see if we're over the limit in the XZ axys without doing any movement
 	# if we are, deccelerate at a custom pace, and do nothing more
-	if round(xyz_to_xz(velocity).length()) > MAX_SPEED:
-		velocity = move_toward_xz(velocity, Vector3.ZERO, DECEL_SPEED / 4)
+	if round(XZ.xz_length(velocity)) > max_speed:
+		velocity = XZ.move_toward_xz(velocity, Vector3.ZERO, decel_speed / 4)
 		return
 
+	velocity += _movement.rotated(Vector3.UP, head_node.rotation.y)
+	if XZ.xz_length(velocity) > max_speed:
+		velocity = XZ.normalize_xz(velocity, max_speed)
 
-	velocity += _movement.rotated(Vector3.UP, head.rotation.y)
-	if xyz_to_xz(velocity).length() > MAX_SPEED:
-		velocity = normalize_xz(velocity, MAX_SPEED)
+	if (
+		is_on_floor() and (not (_up or _down or _left or _right))
+		or ((_up and _down) or (_right and _left))
+	):
+		velocity = XZ.move_toward_xz(velocity, Vector3.ZERO, decel_speed)
 
-	if is_on_floor() and (not (_up or _down or _left or _right)) or ((_up and _down) or (_right and _left)):
-		velocity = move_toward_xz(velocity, Vector3.ZERO, DECEL_SPEED)
+
+func _pushback(node: Node3D) -> void:
+	node.queue_free()
+	velocity.x += sign(abs(node.position.x) - abs(position.x)) * max_speed
+	if node.position.z > position.z:
+		velocity.z += sign(abs(node.position.z) - abs(position.z)) * max_speed
+	else:
+		velocity.z -= sign(abs(node.position.z) - abs(position.z)) * max_speed
+
+
+func _stomp(node: Node3D) -> void:
+	node.queue_free()
+	# If not pressing anything, halt and bounce up
+	if not (_up or _down or _left or _right):
+		velocity = Vector3(0, jump_force * 1.5, 0)
+		return
+	velocity *= jump_speed_boost
+	velocity.y = jump_force
+
+
+func _check_for_bubbles() -> void:
+	var pushbacks: Array[Node3D]
+	var stomps: Array[Node3D]
+
+	pushbacks = push_area.get_overlapping_bodies()
+	pushbacks = pushbacks.filter(func(b: Node3D) -> bool: return b.is_in_group("Bubble"))
+	for b in pushbacks:
+		_pushback(b)
+
+	stomps = stomp_area.get_overlapping_bodies()
+	stomps = stomps.filter(func(s: Node3D) -> bool: return s.is_in_group("Bubble"))
+	for s in stomps:
+		_stomp(s)
+
 
 func _process(_delta: float) -> void:
 	if not is_multiplayer_authority():
-		process_mode = ProcessMode.PROCESS_MODE_DISABLED
+		return
 
-	if (is_on_floor()): velocity.y = 0
-	else: velocity.y -= 1
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y -= gravity
 
-	if (is_on_floor() and Input.is_key_pressed(KEY_SPACE)):
-		velocity *= 2
-		velocity.y += JUMP_FORCE
+	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
+		velocity.y += jump_force
 
-	calculate_horizontal_movement()
-
-	var pushbacks = push_area.get_overlapping_bodies()
-	if pushbacks.size() != 0:
-		for b in pushbacks:
-			if b.is_in_group("Bubble"):
-				velocity.x += sign(abs(b.position.x) - abs(position.x))*MAX_SPEED
-				if (b.position.z > position.z):
-					velocity.z += sign(abs(b.position.z) - abs(position.z))*MAX_SPEED
-				else: velocity.z -= sign(abs(b.position.z) - abs(position.z))*MAX_SPEED
-
-	var stomps = stomp_area.get_overlapping_bodies()
-	if stomps.size() != 0:
-		for b in stomps:
-			if b.is_in_group("Bubble"):
-				velocity *= 3
-				velocity.y = JUMP_FORCE
+	_calculate_horizontal_movement()
+	_check_for_bubbles()
 
 	move_and_slide()
 	rpc("remote_set_position", global_position)
 
+
 @rpc("unreliable")
-func remote_set_position(real_pos):
+func remote_set_position(real_pos: Vector3) -> void:
 	global_position = real_pos
 
+
+func toggle_mouse_capture() -> void:
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
 func _process_keyboard(ev: InputEventKey) -> void:
-	if ev.keycode == KEY_ESCAPE and ev.pressed:
-		if (Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		else: Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	match ev.keycode:
+		KEY_W, KEY_UP:
+			_up = ev.pressed
+		KEY_A, KEY_LEFT:
+			_left = ev.pressed
+		KEY_S, KEY_DOWN:
+			_down = ev.pressed
+		KEY_D, KEY_RIGHT:
+			_right = ev.pressed
 
-	match (ev.keycode):
-		KEY_W, KEY_UP: _up = ev.pressed
-		KEY_A, KEY_LEFT: _left = ev.pressed
-		KEY_S, KEY_DOWN: _down = ev.pressed
-		KEY_D, KEY_RIGHT: _right = ev.pressed
+	if ev.pressed:
+		match ev.keycode:
+			KEY_ESCAPE:
+				toggle_mouse_capture()
 
-func _input(event):
+
+func _process_mouse_motion(ev: InputEventMouseMotion) -> void:
+	var move_x: float = ev.relative.x
+	var move_y: float = ev.relative.y
+	var head_body_diff: float
+
+	head_node.rotate_y(-move_x * p_cam_sensitivity)
+	spring_arm.rotate_x(-move_y * p_cam_sensitivity)
+	spring_arm.rotation.x = clamp(spring_arm.rotation.x, -.8, 1)
+	head_mesh.rotation.x = spring_arm.rotation.x
+
+	head_body_diff = angle_difference(head_node.rotation.y, body_mesh.rotation.y)
+	if abs(head_body_diff) > .7:
+		body_mesh.rotate_y(sign(head_body_diff) * (-abs(head_body_diff) + .7))
+
+
+func _shoot_bubble() -> void:
+	var bubble: Node3D = BUBBLE.instantiate()
+	get_tree().get_root().add_child(bubble)
+	var bubble_dir: Vector3 = -Vector3(
+		head_node.basis.z.x, spring_arm.basis.z.y, head_node.basis.z.z
+	)
+	bubble.position = position + bubble_dir * 5
+	bubble.direction = bubble_dir
+
+
+func _process_mouse_button(ev: InputEventMouseButton) -> void:
+	if ev.pressed && ev.button_index == MOUSE_BUTTON_LEFT:
+		print("Shooty mc shoot shoot")
+		_shoot_bubble()
+
+
+func _input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
 	if event is InputEventKey:
-		_process_keyboard(event); return
+		_process_keyboard(event)
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		spring_arm.rotate_x(-event.relative.y * SENSITIVITY)
-		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -.8, .8)
-
-		head_mesh.rotation.x = spring_arm.rotation.x
-
-		var head_body_diff = angle_difference(head.rotation.y, body.rotation.y)
-		if (abs(head_body_diff) > .7):
-			body.rotate_y(sign(head_body_diff) * (-abs(head_body_diff) + .7))
-			
+		_process_mouse_motion(event)
+	if event is InputEventMouseButton:
+		_process_mouse_button(event)
--- Networking/network.gd
+++ Networking/network.gd
@@ -1,6 +1,24 @@
 extends Node
 
-var multiplayer_peer = ENetMultiplayerPeer.new()
+const PORT: int = 1221
+
+var multiplayer_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+
+var connected_peer_ids: Array[int] = []
+var good_guys: Array[int] = []
+var bad_guys: Array[int] = []
+
+@onready var good_guys_spawn: Node3D = $World/GoodGuysSpawn
+@onready var bad_guys_spawn: Node3D = $World/BadGuysSpawn
+@onready var join_lobby_cont: VBoxContainer = $Control/JoinLobby
+@onready var join_btn: Button = $Control/JoinLobby/join
+@onready var host_btn: Button = $Control/JoinLobby/host
+@onready var status_container: VBoxContainer = $Control/Status
+@onready var status_label: Label = $Control/Status/Status
+@onready var id_label: Label = $Control/Status/Id
+@onready var join_team_container: VBoxContainer = $Control/JoinTeam
+@onready var join_good_btn: Button = $Control/JoinTeam/JoinGood
+@onready var join_bad_btn: Button = $Control/JoinTeam/JoinBad
 
 # # Called when the node enters the scene tree for the first time.
 # func _ready() -> void:
@@ -23,49 +41,109 @@ var multiplayer_peer = ENetMultiplayerPeer.new()
 # 	upnp.delete_port_mapping(1221, "TCP")
 # 	upnp.delete_port_mapping(1221, "UDP")
 
-const PORT = 1221
-
-var connected_peer_ids = []
-
-func _on_host_pressed() -> void:
-	$Control/VBoxContainer/Status.text = "Server"
-	multiplayer_peer.create_server(1221)
-	multiplayer.multiplayer_peer = multiplayer_peer
-	$Control/VBoxContainer/Id.text = str(multiplayer.get_unique_id())
-	
-	$Control/VBoxContainer/join.visible = false
-	$Control/VBoxContainer/host.visible = false
-
-	add_player_character(1)
-
-	multiplayer.peer_connected.connect(
-		func (id):
-			rpc("add_new_connections", id)
-			rpc_id(id, "add_previous_characters", connected_peer_ids)
-			add_player_character(id)
-	)
-
 @rpc
-func add_new_connections(id):
+func add_new_connections(id: int) -> void:
+	connected_peer_ids.append(id)
 	add_player_character(id)
 
+
 @rpc
-func add_previous_characters(ids):
+func add_previous_characters(ids: Array[int]) -> void:
 	for peer_id in ids:
 		add_player_character(peer_id)
 
+
+@rpc
+func show_join_team() -> void:
+	join_team_container.visible = true
+
+
+@rpc("any_peer", "reliable")
+func handle_team_join(id: int, is_good: bool) -> void:
+
+	# Host registers if someone is good or bad
+	if is_good:
+		good_guys.append(id)
+	else:
+		bad_guys.append(id)
+
+	# Host tells everyone, this guy joined
+	print("SENDING ADD NEW TO ALL PLAYERS")
+	rpc("add_new_connections", id)
+
+	# Host tells the player that just joined, these guys are in the game
+	print("SENDING PREVIOUS CHARACTERS TO PEER")
+	rpc_id(id, "add_previous_characters", connected_peer_ids)
+
+	add_player_character(id)
+
+
+
+func _ready() -> void:
+	join_good_btn.connect("pressed", join_good_team)
+	join_bad_btn.connect("pressed", join_bad_team)
+
+
+func join_good_team() -> void:
+	var id: int = multiplayer.get_unique_id()
+	good_guys.append(id)
+	join_team_container.visible = false
+
+	if id == 1:
+		add_player_character(1)
+	else:
+		rpc_id(1, "handle_team_join", id, true)
+	connected_peer_ids.append(id)
+
+
+func join_bad_team() -> void:
+	var id: int = multiplayer.get_unique_id()
+	bad_guys.append(id)
+	join_team_container.visible = false
+
+	if id == 1:
+		add_player_character(1)
+	else:
+		rpc_id(1, "handle_team_join", id, false)
+	connected_peer_ids.append(id)
+
+
+func _on_host_pressed() -> void:
+	multiplayer_peer.create_server(1221)
+	multiplayer.multiplayer_peer = multiplayer_peer
+
+	join_lobby_cont.visible = false
+	status_label.text = "Server"
+	id_label.text = str(multiplayer.get_unique_id())
+
+	show_join_team()
+
+	multiplayer.peer_connected.connect(
+		# This function runs IN THE HOST when someone joins
+		# Send them instructions with the rpc_id() function
+		func(id: int) -> void:
+			print("Connection...")
+			rpc_id(id, "show_join_team")
+	)
+
+
 func _on_join_pressed() -> void:
-	$Control/VBoxContainer/Status.text = "Client"
 	multiplayer_peer.create_client("localhost", PORT)
 	multiplayer.multiplayer_peer = multiplayer_peer
-	$Control/VBoxContainer/Id.text = str(multiplayer.get_unique_id())
 
-	$Control/VBoxContainer/join.visible = false
-	$Control/VBoxContainer/host.visible = false
+	join_lobby_cont.visible = false
+	status_label.text = "Client"
+	id_label.text = str(multiplayer.get_unique_id())
 
-func add_player_character(id):
-	connected_peer_ids.append(id)
-	var p_instance = preload("res://Networking/test_character.tscn").instantiate()
+
+func add_player_character(id: int) -> void:
+	var p_instance: CharacterBody3D = preload("res://Networking/test_character.tscn").instantiate()
 	p_instance.set_multiplayer_authority(id)
-	p_instance.position.y = 10;
+
+	var is_good: bool = good_guys.has(id)
+	p_instance.is_good = is_good
+	var spawn: Node3D = good_guys_spawn if is_good else bad_guys_spawn
+	p_instance.position = spawn.position
+	p_instance.position.y = spawn.position.y + 3
+
 	add_child(p_instance)
