dbbcabe5e46d9f13f5e6defadc6f84a611a47cbe
Kimbix <alemanhumberto06@gmail.com>
2025-01-24 01:53:36 -0400

    Movement script now rotates head and body

--- Networking/Movement.gd
+++ Networking/Movement.gd
@@ -1,20 +1,29 @@
 extends CharacterBody3D
 
+const DEG_45: float = PI/4.0
+const DEG_N45: float = -DEG_45
+
+@onready var body: Node = $Body
 @onready var head: Node = $Head
+@onready var head_mesh: Node = $Head/HeadMesh
 @onready var spring_arm: SpringArm3D = $Head/SpringArm3D
 @onready var p_cam: Camera3D = $Head/SpringArm3D/PlayerCamera
+
+@export var SPEED: float = 1.5
+@export var DECEL_SPEED: float = 3.0
+@export var MAX_SPEED: float = 20.0
+
+@export var JUMP_FORCE: float = 50.0
+
+@export var SENSITIVITY = 0.01
+
+## Lower is stronger
+@export var BODY_ROTATION_STRENGTH = 10.0
 
 func _ready() -> void:
 	name = str(get_multiplayer_authority())
 	p_cam.current = is_multiplayer_authority()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-const DEG_45: float = PI/4.0
-const DEG_N45: float = -DEG_45
-
-const SPEED: float = 1.0
-const DECEL_SPEED: float = 2.0
-const MAX_SPEED: float = 15.0
 
 var _up: bool = Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W)
 var _down: bool = Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S)
@@ -36,7 +45,14 @@ func calculate_horizontal_movement() -> void:
 	if _down and not _up: _movement += Vector3.BACK * SPEED
 	if _right and not _left: _movement += Vector3.RIGHT * SPEED
 	if _left and not _right: _movement += Vector3.LEFT * SPEED
+
+	if _movement.z < 0:
+		body.rotate_y(angle_difference(body.rotation.y, head.rotation.y)/10.0)
+	if _movement.x:
+		body.rotate_y(angle_difference(body.rotation.y + sign(_movement.x) * DEG_45, head.rotation.y)/10.0)
+
 	velocity += _movement.rotated(Vector3.UP, head.rotation.y)
+
 
 	if xyz_to_xz(self.velocity).length() > MAX_SPEED:
 		velocity = normalize_xz(velocity, MAX_SPEED)
@@ -51,7 +67,7 @@ func _process(_delta: float) -> void:
 	else: velocity.y -= 1
 
 	if (is_on_floor() and Input.is_key_pressed(KEY_SPACE)):
-		velocity.y += 40
+		velocity.y += JUMP_FORCE
 
 	calculate_horizontal_movement()
 	move_and_slide()
@@ -60,11 +76,6 @@ func _process(_delta: float) -> void:
 @rpc("unreliable")
 func remote_set_position(real_pos):
 	global_position = real_pos
-
-var mouse_sens = 0.3
-var camera_anglev=0
-
-const SENSITIVITY = 0.01
 
 func _process_keyboard(ev: InputEventKey) -> void:
 	if ev.keycode == KEY_ESCAPE and ev.pressed:
@@ -84,4 +95,11 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		spring_arm.rotate_x(-event.relative.y * SENSITIVITY)
-		spring_arm.rotation.x =clamp(spring_arm.rotation.x, -1, .2)
+		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -.8, .8)
+
+		head_mesh.rotation.x = spring_arm.rotation.x
+
+		var head_body_diff = angle_difference(head.rotation.y, body.rotation.y)
+		if (abs(head_body_diff) > .7):
+			body.rotate_y(sign(head_body_diff) * (-abs(head_body_diff) + .7))
+			
