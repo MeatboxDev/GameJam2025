58b8a3b4604043cd2ae6fb2954e07d194321c0a2
Ruskki <theawsomegirl2004@gmail.com>
2025-01-25 18:14:39 -0400

    map - corners + player capsule hitbox

---
+++ Assets/Maps/Ads/ad_display.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+var _ads : Array [Sprite3D] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_ads.append($Ad_mount_collisions/Sprite_01)
+	_ads.append($Ad_mount_collisions/Sprite_02)
+	
+	_ads.pick_random().show()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
