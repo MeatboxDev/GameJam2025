8b04f449aedc85dced8ac1d8adfbb4b09cdf30ea
Kimbix <alemanhumberto06@gmail.com>
2025-01-24 21:38:28 -0400

    boss balls now transmit signals about doing SHIT

--- Testing/bubble.gd
+++ Testing/bubble.gd
@@ -1,30 +1,29 @@
 extends StaticBody3D
 
+@export var is_good: bool = false
 @export var direction: Vector3 = Vector3.ZERO
 @export var speed: float = 0.15
 @export var decel_speed: float = 0.0005
 @export var decel_start_time: float = 0.2
-
 @export var magnitude: float = PI/6
 
-var height_mod: float = 0
-
-var start_deccel: bool = false
+var _height_mod: float = 0
+var _start_deccel: bool = false
 
 
 func _ready() -> void:
 	await get_tree().create_timer(decel_start_time).timeout
-	start_deccel = true
+	_start_deccel = true
 	speed = 0.1
 
 
 func _process(delta: float) -> void:
-	position.y = position.y + sin(height_mod) / 256
-	height_mod += delta
+	position.y = position.y + sin(_height_mod) / 256
+	_height_mod += delta
 
 	if speed < 0: return
 	position += direction * speed
-	if not start_deccel:
+	if not _start_deccel:
 		return
 	speed -= decel_speed
 
