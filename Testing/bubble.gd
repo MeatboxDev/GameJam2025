4569dc772c33d51357258bbdc8169e6681826ab8
Kimbix <alemanhumberto06@gmail.com>
2025-01-25 16:30:04 -0400

    I hate bubbles

--- Testing/bubble.gd
+++ Testing/bubble.gd
@@ -13,6 +13,13 @@ var _start_deccel: bool = false
 @onready var area: Area3D = $Area
 
 
+func _handle_body_exited(_body: Node3D) -> void:
+	if collision_layer != 0:
+		return
+	collision_layer = 1
+	collision_mask = 1
+
+
 func _handle_area_collision(_area: Area3D) -> void:
 	var par: Node3D = _area.get_parent()
 	if par == null:
@@ -26,9 +33,14 @@ func _handle_area_collision(_area: Area3D) -> void:
 
 func _on_timeout() -> void:
 	_start_deccel = true
+	collision_layer = 1
+	collision_mask = 1
 
 func _ready() -> void:
+	multiplayer.allow_object_decoding = true
+	name += "BUBBLE"
 	area.connect("area_entered", _handle_area_collision)
+	area.connect("body_exited", _handle_body_exited)
 	var tween: Tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_ELASTIC)
@@ -38,36 +50,67 @@ func _ready() -> void:
 	timer.connect("timeout", _on_timeout)
 
 
-func _process(delta: float) -> void:
-	position.y = position.y + sin(_height_mod) / 256
-	_height_mod += delta
+@rpc("reliable")
+func net_pushback(collider: Node3D) -> void:
+	collider.pushback(self)
 
+
+@rpc("reliable")
+func net_stomp(collider: Node3D) -> void:
+	collider.stomp()
+
+
+func _check_for_players(col: KinematicCollision3D) -> void:
+	if col == null:
+		return
+	for c in col.get_collision_count():
+		var collider: Node3D = col.get_collider(c)
+		if collider == null:
+			return
+		if not collider.is_in_group("Player"):
+			return
+		var norm: Vector3 = col.get_normal(c)
+		if norm.y > 0 and abs(norm.y) > abs(norm.x) and abs(norm.y) > abs(norm.z):
+			rpc("net_stomp", collider)
+			collider.stomp()
+			rpc("burst")
+			return
+		rpc("net_pushback", collider)
+		collider.pushback(self)
+		rpc("burst")
+
+
+func _process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
 
-	if speed < 0:
-		return
-	var col: KinematicCollision3D = move_and_collide(direction * speed)
+	position.y = position.y + sin(_height_mod) / 256
+	_height_mod += delta
+
+	speed = max(speed, 0)
+	var col: KinematicCollision3D = move_and_collide(direction * speed, false, 0, true)
+	# _check_for_players(col)
 	if col:
-		if col.get_collider().is_in_group("Terrain"):
-			if col.get_normal().x: direction.x *= -1
-			if col.get_normal().y: direction.y *= -1
-			if col.get_normal().z: direction.z *= -1
+		for c: int in col.get_collision_count():
+			if col.get_collider(c).is_in_group("Terrain"):
+				if col.get_normal().x:
+					direction.x *= -1
+				if col.get_normal().y:
+					direction.y *= -1
+				if col.get_normal().z:
+					direction.z *= -1
+
+	rpc("_net_update_position", global_position)
 	if not _start_deccel:
 		return
 	speed -= decel_speed
-	rpc("_net_update_position", global_position)
+
 
 @rpc("unreliable")
 func _net_update_position(real_pos: Vector3) -> void:
 	global_position = real_pos
 
 
-@rpc("any_peer", "reliable")
-func _net_burst() -> void:
-	self.queue_free()
-
-
+@rpc("any_peer", "call_local", "reliable")
 func burst() -> void:
-	rpc("_net_burst")
 	self.queue_free()
