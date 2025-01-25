c7c9fea016d8f7d55f18e8430f7612b704809fdd
Kimbix <alemanhumberto06@gmail.com>
2025-01-25 16:30:04 -0400

    I hate bubbles

--- Networking/Movement.gd
+++ Networking/Movement.gd
@@ -17,6 +17,7 @@ const XZ = preload("res://Networking/xyz_to_xz.gd")
 @export var gravity: float = 0.98
 
 var is_good: bool = true
+var is_alive: bool = true
 
 var _up: bool = Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W)
 var _down: bool = Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S)
@@ -24,8 +25,14 @@ var _right: bool = Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D
 var _left: bool = Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A)
 var _movement: Vector3 = Vector3.ZERO
 
-@onready var stomp_area: Area3D = $StompArea
-@onready var push_area: Area3D = $PushArea
+var _health: float = 100:
+	get():
+		return _health
+	set(val):
+		if val < 0:
+			rpc("_trigger_defeat")
+		_health = val
+
 @onready var body_mesh: Node = $Body
 @onready var head_node: Node = $Head
 @onready var head_mesh: Node = $Head/HeadMesh
@@ -33,17 +40,58 @@ var _movement: Vector3 = Vector3.ZERO
 @onready var p_cam: Camera3D = $Head/SpringArm3D/PlayerCamera
 @onready var bubble_cast: ShapeCast3D = $BubbleCast
 
+@rpc("any_peer", "call_local", "reliable")
+func trigger_respawn() -> void:
+	collision_layer = 1
+	collision_mask = 1
+
+	var tween: Tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(self, "scale", Vector3.ONE, 0.5)
+	is_alive = true
+
+
+@rpc("any_peer", "call_local", "reliable")
+func _trigger_defeat() -> void:
+	collision_layer = 2
+	collision_mask = 2
+
+	var tween: Tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector3.ONE * 0.01, 1)
+	is_alive = false
+
+	_up = false
+	_down = false
+	_left = false
+	_right = false
+
+	pushback_pos(position - p_cam.basis.z * 2)
+	SignalBus.player_defeat.emit(self)
+
+
+func get_all_children(in_node: Node3D, arr: Array[Node3D]) -> Array[Node3D]:
+	arr.push_back(in_node)
+	for child in in_node.get_children():
+		arr = get_all_children(child, arr)
+	return arr
+
 
 func _ready() -> void:
 	name = str(get_multiplayer_authority())
 	p_cam.current = is_multiplayer_authority()
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-	for c: Node3D in $Model.get_children():
-		for mesh: MeshInstance3D in c.get_children():
-			var new_material: StandardMaterial3D = StandardMaterial3D.new()
-			new_material.albedo_color = "0000ff" if is_good else "ff0000"
-			mesh.material_override = new_material
+	if is_multiplayer_authority():
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	for c: Node3D in get_all_children(self, []).filter(
+		func(n: Node3D) -> bool: return n is MeshInstance3D
+	):
+		var new_material: StandardMaterial3D = StandardMaterial3D.new()
+		new_material.albedo_color = "0000ff" if is_good else "ff0000"
+		c.material_override = new_material
 
 
 func _calculate_horizontal_movement() -> void:
@@ -74,7 +122,7 @@ func _calculate_horizontal_movement() -> void:
 
 	# First we check to see if we're over the limit in the XZ axys without doing any movement
 	# if we are, deccelerate at a custom pace, and do nothing more
-	if round(XZ.xz_length(velocity)) > max_speed:
+	if round(XZ.xz_length(velocity)) > max_speed or not is_alive:
 		velocity = XZ.move_toward_xz(velocity, Vector3.ZERO, decel_speed / 4)
 		return
 
@@ -89,20 +137,24 @@ func _calculate_horizontal_movement() -> void:
 		velocity = XZ.move_toward_xz(velocity, Vector3.ZERO, decel_speed)
 
 
-func _pushback(node: Node3D) -> void:
-	if not node.is_in_group("BossBubble"):
-		node.burst()
+func pushback_pos(pos: Vector3) -> void:
+	velocity.x = -(pos.x - move_toward(position.x, pos.x, 1)) * max_speed
+	velocity.z = -(pos.z - move_toward(position.z, pos.z, 1)) * max_speed
+
+
+func pushback(node: Node3D) -> void:
 	velocity.x = -(node.position.x - move_toward(position.x, node.position.x, 1)) * max_speed
 	velocity.z = -(node.position.z - move_toward(position.z, node.position.z, 1)) * max_speed
+	if node.is_good != is_good:
+		_health -= 33.33
 
 
-func _stomp(node: Node3D) -> void:
-	if not node.is_in_group("BossBubble"):
-		node.burst()
-
+func stomp() -> void:
 	# If not pressing anything, halt and bounce up
 	if not (_up or _down or _left or _right):
 		velocity = Vector3(0, jump_force * 1.5, 0)
+		move_and_slide()
+		force_update_transform()
 		return
 	velocity *= jump_speed_boost
 	velocity.y = jump_force
@@ -118,10 +170,12 @@ func _check_for_bubbles() -> void:
 			return
 		if not collider.is_in_group("Bubble"):
 			return
-		var norm: Vector3 = col.get_normal(c)
-		if norm.y != 0:
-			return _stomp(collider)
-		return _pushback(collider)
+		if col.get_normal(c).y != 0:
+			stomp()
+		else:
+			pushback(collider)
+		if not collider.is_in_group("BossBubble"):
+			collider.rpc("burst")
 
 
 func _process(_delta: float) -> void:
@@ -139,17 +193,26 @@ func _process(_delta: float) -> void:
 	_calculate_horizontal_movement()
 	_check_for_bubbles()
 	move_and_slide()
+	if XZ.xz_length(velocity):
+		p_cam.fov = clamp(p_cam.fov + _delta * 100, 75, 100)
+	else:
+		p_cam.fov = clamp(p_cam.fov - _delta * 100, 75, 100)
 
 	rpc("remote_set_position", global_position)
 	rpc("remote_set_head_rotation", head_node.rotation, spring_arm.rotation, velocity)
 
 
-@rpc("unreliable")
+@rpc("any_peer", "unreliable")
 func remote_set_position(real_pos: Vector3) -> void:
 	global_position = real_pos
 
 
 @rpc("unreliable")
+func _net_shoot_bubble() -> void:
+	_shoot_bubble()
+
+
+@rpc("any_peer", "unreliable")
 func remote_set_head_rotation(
 	real_rotation: Vector3, real_spring_rotation: Vector3, real_velocity: Vector3
 ) -> void:
@@ -157,18 +220,6 @@ func remote_set_head_rotation(
 	head_node.rotation = real_rotation
 	head_mesh.rotation = real_rotation
 	spring_arm.rotation = real_spring_rotation
-
-
-@rpc("any_peer", "reliable")
-func _net_shoot_bubble() -> void:
-	_shoot_bubble()
-
-
-func toggle_mouse_capture() -> void:
-	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	else:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _process_keyboard(ev: InputEventKey) -> void:
@@ -181,11 +232,6 @@ func _process_keyboard(ev: InputEventKey) -> void:
 			_down = ev.pressed
 		KEY_D, KEY_RIGHT:
 			_right = ev.pressed
-
-	if ev.pressed:
-		match ev.keycode:
-			KEY_ESCAPE:
-				toggle_mouse_capture()
 
 
 func _process_mouse_motion(ev: InputEventMouseMotion) -> void:
@@ -207,25 +253,25 @@ func _process_mouse_motion(ev: InputEventMouseMotion) -> void:
 
 
 func _shoot_bubble() -> void:
-	var bubble: Node3D = (BUBBLE_GOOD if is_good else BUBBLE_BAD).instantiate()
-	get_tree().get_root().add_child(bubble)
-
-	bubble_cast.position = head_node.position + (-p_cam.global_basis.z) * 3
-	bubble_cast.force_update_transform()
-	bubble_cast.force_shapecast_update()
 	var arr: Array = bubble_cast.collision_result
 	for x: Dictionary in arr:
 		var obj: Node3D = x["collider"]
-		if not obj.is_in_group("Terrain"):
-			continue
-		bubble.queue_free()
-		return
+		if obj.is_in_group("Terrain"):
+			return
 
-	bubble.position = position + bubble_cast.position
+	var bubble: Node3D = (BUBBLE_GOOD if is_good else BUBBLE_BAD).instantiate()
+
+	bubble.position = bubble_cast.global_position - (p_cam.global_basis.z * 2)
 	bubble.direction = (-p_cam.global_basis.z)
 	bubble.scale = Vector3.ONE * 0.01
-	bubble.speed += (bubble.direction.normalized().dot(velocity.normalized())) / 2
-	bubble.set_multiplayer_authority(get_multiplayer_authority())
+
+	var dot: float = bubble.direction.normalized().dot(velocity.normalized())
+	if dot > .5 or dot < -.5:
+		bubble.speed *= 1 + bubble.direction.normalized().dot(velocity.normalized()) / 2
+	if velocity.y != 0:
+		bubble.speed *= 1 + abs(velocity.normalized().y)
+	bubble.set_multiplayer_authority(1)
+	get_tree().get_root().add_child(bubble)
 
 
 func _process_mouse_button(ev: InputEventMouseButton) -> void:
@@ -236,6 +282,8 @@ func _process_mouse_button(ev: InputEventMouseButton) -> void:
 
 func _input(event: InputEvent) -> void:
 	if not is_multiplayer_authority():
+		return
+	if not is_alive:
 		return
 	if event is InputEventKey:
 		_process_keyboard(event)
