d86675fa8b215bc71badc9f3895589549e007b45
Kimbix <alemanhumberto06@gmail.com>
2025-01-26 01:47:33 -0400

    Added restrictions on shooting

--- Networking/Movement.gd
+++ Networking/Movement.gd
@@ -13,6 +13,10 @@ const HURT_SOUNDS := [
 	preload("res://Assets/SoundEffects/herida_3.wav"),
 ]
 
+const SHOOT_COOLDOWN := .5
+const INVIS_TIME := 1.5
+const FLASH_TIMES := 18
+
 const BUBBLE_GOOD: PackedScene = preload("res://Testing/bubble_purple.tscn")
 const BUBBLE_BAD: PackedScene = preload("res://Testing/bubble_pink.tscn")
 const DEG_45: float = PI / 4.0
@@ -41,6 +45,10 @@ var health: float = 100:
 			rpc("_trigger_defeat")
 		health = val
 
+var _is_shooting := false
+var _shoot_disabled := false
+var _invincible := false
+var _flash_count := 0
 var _last_player_hurt_sound: AudioStream = null
 var _up: bool = Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_W)
 var _down: bool = Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_S)
@@ -60,6 +68,7 @@ var _movement: Vector3 = Vector3.ZERO
 
 @rpc("any_peer", "call_local", "reliable")
 func trigger_respawn() -> void:
+	velocity = Vector3.ZERO
 	collision_layer = 1
 	collision_mask = 1
 
@@ -67,9 +76,7 @@ func trigger_respawn() -> void:
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_BACK)
 	tween.tween_property(self, "scale", Vector3.ONE, 1)
-	tween.tween_callback(func() -> void:
-		audio_player.stop()
-	)
+	tween.tween_callback(func() -> void: audio_player.stop())
 
 	is_alive = true
 	audio_player.stream = preload("res://Assets/SoundEffects/respawn.wav")
@@ -174,19 +181,34 @@ func pushback_pos(pos: Vector3) -> void:
 	velocity.z = -(pos.z - move_toward(position.z, pos.z, 1)) * max_speed
 
 
+func _flash_function() -> void:
+	if _flash_count == 0:
+		_invincible = false
+		visible = true
+		return
+	_flash_count -= 1
+	visible = !visible
+	get_tree().create_timer(INVIS_TIME / FLASH_TIMES).timeout.connect(_flash_function)
+
+
 @rpc("any_peer", "reliable", "call_local")
 func pushback(node: Node3D) -> void:
 	velocity.x = -(node.position.x - move_toward(position.x, node.position.x, 1)) * max_speed
 	velocity.z = -(node.position.z - move_toward(position.z, node.position.z, 1)) * max_speed
 
-	var to_play: AudioStream = _last_player_hurt_sound
-	while to_play == _last_player_hurt_sound:
-		to_play = HURT_SOUNDS.pick_random()
-	audio_player.stream = to_play
-	audio_player.play()
-	audio_player.pitch_scale = voice_pitch
-	_last_player_hurt_sound = to_play
-	if node.is_good != is_good:
+	if not _invincible and node.is_good == is_good:
+		var to_play: AudioStream = _last_player_hurt_sound
+		while to_play == _last_player_hurt_sound:
+			to_play = HURT_SOUNDS.pick_random()
+		audio_player.stream = to_play
+		audio_player.play()
+		audio_player.pitch_scale = voice_pitch
+		_last_player_hurt_sound = to_play
+
+		_invincible = true
+		_flash_count = FLASH_TIMES
+		get_tree().create_timer(INVIS_TIME / FLASH_TIMES).timeout.connect(_flash_function)
+
 		health -= 33.33
 
 
@@ -235,6 +257,14 @@ func _jump() -> void:
 	rpc("_net_jump")
 
 
+func _attempt_shoot() -> void:
+	if _invincible or _shoot_disabled:
+		return
+	_shoot_disabled = true
+	get_tree().create_timer(SHOOT_COOLDOWN).timeout.connect(func() -> void: _shoot_disabled = false)
+	rpc("_net_shoot_bubble")
+
+
 func _process(_delta: float) -> void:
 	if not is_alive:
 		p_cam.top_level = true
@@ -245,6 +275,9 @@ func _process(_delta: float) -> void:
 
 	if not is_multiplayer_authority() or not is_alive:
 		return
+
+	if _is_shooting:
+		_attempt_shoot()
 
 	if is_on_floor():
 		velocity.y = 0
@@ -360,14 +393,12 @@ func _shoot_bubble() -> void:
 
 
 func _process_mouse_button(ev: InputEventMouseButton) -> void:
-	if ev.pressed && ev.button_index == MOUSE_BUTTON_LEFT:
-		rpc("_net_shoot_bubble")
+	if ev.button_index == MOUSE_BUTTON_LEFT:
+		_is_shooting = ev.pressed
 
 
 func _input(event: InputEvent) -> void:
-	if not is_multiplayer_authority():
-		return
-	if not is_alive:
+	if not is_multiplayer_authority() or not is_alive:
 		return
 	if event is InputEventKey:
 		_process_keyboard(event)
