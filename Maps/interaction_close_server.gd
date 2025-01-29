dbe90d92c8adf8174656b92815390e9598f92f9a
Kimbix <alemanhumberto06@gmail.com>
2025-01-29 06:47:30 -0400

    fix: Removed individual interaction files

--- Maps/interaction_close_server.gd
+++
@@ -1,9 +0,0 @@
-class_name Interaction extends Node
-
-@export var _lobby_controller: LobbyController
-
-func _ready() -> void:
-	assert(_lobby_controller, "Lobby Controller not set")
-
-func interact() -> void:
-	_lobby_controller.create_server()
