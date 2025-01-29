71154329d7bb90b63a8f533f5094564f97993060
Kimbix <alemanhumberto06@gmail.com>
2025-01-29 17:20:42 -0400

    feat: basic idle, run, jump

--- Player/state_idle.gd
+++ Player/state_idle.gd
@@ -1,8 +1,13 @@
 extends State
 
+const JUMP_FORCE := 30.0
+const DECELERATION := 4.0
+
+@export var body: CharacterBody3D
+
 
 func on_set() -> void:
-	pass
+	assert(body)
 
 
 func on_leave() -> void:
@@ -14,7 +19,18 @@ func update() -> void:
 
 
 func physics_update() -> void:
-	pass  # TODO: Add gravity lmao
+	if not body.is_on_floor():
+		transition.emit(self, "jumping")  # TODO: Create and change to a falling state
+
+	body.velocity = Vector3(
+		move_toward(body.velocity.x, 0, DECELERATION),
+		body.velocity.y,
+		move_toward(body.velocity.z, 0, DECELERATION),
+	)
+	if Input.is_key_pressed(KEY_SPACE):
+		transition.emit(self, "jumping")
+
+	body.move_and_slide()
 
 
 func input(_event: InputEvent) -> void:
