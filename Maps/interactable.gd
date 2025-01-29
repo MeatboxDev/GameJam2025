c8e892af62907a664f04175c36323162e549e3fe
Kimbix <alemanhumberto06@gmail.com>
2025-01-29 06:48:03 -0400

    chore: made pretty interactable.gd

--- Maps/interactable.gd
+++ Maps/interactable.gd
@@ -8,18 +8,20 @@ enum INTERACTIONS {
 	LEAVE_SERVER = 4,
 }
 
+@export var _interaction: INTERACTIONS
+@export var _lobby_controller: LobbyController
+
 var is_focused: bool = false:
 	get():
 		return is_focused
 	set(val):
 		is_focused = val
-		if val: _on_focus()
-		else: _on_focus_lost()
+		if val:
+			_on_focus()
+		else:
+			_on_focus_lost()
 
-@export var _interaction: INTERACTIONS
-@export  var _lobby_controller :LobbyController
-
-@onready var INTERACTIONS_ARRAY := [
+@onready var interactions_array := [
 	func() -> void: print("Empty interaction!"),
 	_lobby_controller.create_server,
 	_lobby_controller.close_server,
@@ -31,21 +33,27 @@ var is_focused: bool = false:
 @onready var _interaction_info: Sprite3D = $InteractableInformation
 @onready var _interaction_area: Area3D = $InteractableArea
 
+
 func _on_focus() -> void:
 	_interaction_info.visible = true
 	_interaction_info_tween.kill()
-	_interaction_info_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	_interaction_info_tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(
+		Tween.TRANS_BACK
+	)
 	_interaction_info_tween.tween_property(_interaction_info, "scale", Vector3.ONE, 0.33)
 
+
 func _on_focus_lost() -> void:
-	_interaction_info_tween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
-	_interaction_info_tween.tween_property(_interaction_info, "scale", Vector3.ONE * 0.01, 0.33)
-	_interaction_info_tween.tween_callback(func() -> void:
-		_interaction_info.visible = false
+	_interaction_info_tween = get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(
+		Tween.TRANS_BACK
 	)
+	_interaction_info_tween.tween_property(_interaction_info, "scale", Vector3.ONE * 0.01, 0.33)
+	_interaction_info_tween.tween_callback(func() -> void: _interaction_info.visible = false)
+
 
 func interact() -> void:
-	INTERACTIONS_ARRAY[_interaction].call()
+	interactions_array[_interaction].call()
+
 
 func _ready() -> void:
 	assert(_interaction, "Interaction not set!")
