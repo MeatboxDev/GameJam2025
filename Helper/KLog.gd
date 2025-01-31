39579965c8a1c1df79e2ad142e8594d1f5b7e39c
Kimbix <alemanhumberto06@gmail.com>
2025-01-31 05:16:48 -0400

    feat: created KLog.gd for better logging

---
+++ Helper/KLog.gd
@@ -0,0 +1,34 @@
+class_name KLog extends Node
+
+enum LEVEL { CRITICAL, ERROR, WARN, INFO, DEBUG }
+static var log_level := LEVEL.DEBUG
+
+
+## Prints a message in red starting with CRIT: and stops the program
+static func critical(msg: String) -> void:
+	print_rich("`color=red`CRIT: " + msg + "`/color`")
+	breakpoint
+
+
+## Prints a message in red starting with ERROR
+static func error(msg: String) -> void:
+	if log_level >= LEVEL.ERROR:
+		print_rich("[color=red]ERROR: " + msg + "[/color]")
+
+
+## Prints a message in yellow starting with WARN
+static func warning(msg: String) -> void:
+	if log_level >= LEVEL.WARN:
+		print_rich("[color=yellow]WARN: " + msg + "[/color]")
+
+
+## Prints a message in cyan starting with INFO
+static func info(msg: String) -> void:
+	if log_level >= LEVEL.INFO:
+		print_rich("[color=cyan]INFO: " + msg + "[/color]")
+
+
+## Prints a message in green starting with DEBUG
+static func debug(msg: String) -> void:
+	if log_level >= LEVEL.DEBUG:
+		print_rich("[color=green]DEBUG: " + msg + "[/color]")
