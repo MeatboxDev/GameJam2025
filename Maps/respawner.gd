a755e49e6bd19aa67f4dd2f83f2d8d52594ddb49
Kimbix <alemanhumberto06@gmail.com>
2025-02-05 16:33:52 -0400

    fix: fixed bubble shooting on cris's map (how)

--- Maps/respawner.gd
+++ Maps/respawner.gd
@@ -5,10 +5,10 @@ class_name Respawner extends Node
 func _request_spawn_point() -> Vector3:
 	if _spawn_points.size() == 0:
 		return Vector3.ZERO
-	return _spawn_points.front().position
+	return _spawn_points.front().global_position
 	
 func _respawn_player(player: CharacterBody3D) -> void:
-	player.position = _request_spawn_point()
+	player.global_position = _request_spawn_point()
 
 func _init() -> void:
 	SignalBus.respawn_player.connect(_respawn_player)
