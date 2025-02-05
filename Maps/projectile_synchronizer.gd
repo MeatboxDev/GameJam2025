01115ccdeeb2377dc21928a9bea5c98e69a1d513
Kimbix <alemanhumberto06@gmail.com>
2025-02-05 18:20:37 -0400

    fix: bubbles were not properly synchronizing

--- Maps/projectile_synchronizer.gd
+++ Maps/projectile_synchronizer.gd
@@ -2,58 +2,58 @@ extends Node
 
 const _bubble_scene: PackedScene = preload("uid://duhicp2kli703")
 
-var bubble_number := 0
-var _bubble_list: Dictionary = {}
-
+var _bubble_list: Array[Node3D] = []
 
 @rpc("authority", "unreliable", "call_remote")
 func _sync_position(index: int, pos: Vector3) -> void:
-	if not _bubble_list.has(index):
+	var instance: Array[Node3D] = _bubble_list.filter(func(i: Node3D) -> bool: return i.number == index)
+	if not instance.size():
 		KLog.warning("Bubble with index " + str(index) + " not found over the net")
 		return
-	_bubble_list[index].global_position = pos
+	instance.front().global_position = pos
 
 
 @rpc("any_peer", "reliable", "call_local")
 func _net_spawn_bubble(bubble_info: Dictionary) -> void:
 	var bubble_instance := _bubble_scene.instantiate()
 	add_child(bubble_instance)
+	
+	bubble_instance.number = (_bubble_list.back().number + 1) if _bubble_list.size() else 0
 	for k: String in bubble_info:
 		bubble_instance.set(k, bubble_info[k])
-	bubble_instance.number = bubble_number
-	bubble_instance.name = "bubble-" + str(bubble_number)
+	bubble_instance.name = "bubble-" + str(bubble_instance.number)
 	bubble_instance.top_level = true
-	_bubble_list[bubble_number] = bubble_instance
-	bubble_number += 1
 	
+	_bubble_list.append(bubble_instance)
 
 func _burst_bubble(num: int) -> void:
-	var instance: Node3D = _bubble_list[num]
-	if not instance:
+	var instance: Array[Node3D] = _bubble_list.filter(func(i: Node3D) -> bool: return i.number == num)
+	if not instance.size():
 		KLog.warning("Instance of bubble number " + str(num) + " not found, ignoring")
 		return
-	instance.queue_free()
-	_bubble_list.erase(num)
-	KLog.debug("Bursting " + str(num))
+	instance.front().queue_free()
+	_bubble_list.erase(instance.front())
+
 
 func _clear_bubbles() -> void:
-	for i: int in _bubble_list.keys():
-		_bubble_list[i].burst()
-	bubble_number = 0
+	for i: Node3D in _bubble_list.duplicate(false):
+		i.burst()
+
 
 func _ready() -> void:
-	multiplayer.peer_connected.connect(func(id: int) -> void:
-		for i: int in _bubble_list.keys():
-			var instance: StaticBody3D = _bubble_list[i]
-			var info := {}
-			info["collision_layer"] = 2 | 4
-			info["collision_mask"] = 2 | 4
-			info["position"] = instance.global_position
-			info["speed"] = instance.speed
-			info["decceleration"] = instance.decceleration
-			info["direction"] = instance.direction
-			rpc_id(id, "_net_spawn_bubble", info)
-	)
+	if is_multiplayer_authority():
+		multiplayer.peer_connected.connect(func(id: int) -> void:
+			for i: Node3D in _bubble_list:
+				var info := {}
+				info["collision_layer"] = 2 | 4
+				info["collision_mask"] = 2 | 4
+				info["position"] = i.global_position
+				info["speed"] = i.speed
+				info["decceleration"] = i.decceleration
+				info["direction"] = i.direction
+				info["number"] = i.number
+				rpc_id(id, "_net_spawn_bubble", info)
+		)
 	
 	SignalBus.spawn_bubble.connect(func(info: Dictionary) -> void: rpc("_net_spawn_bubble", info))
 	SignalBus.burst_bubble.connect(_burst_bubble)
@@ -64,5 +64,5 @@ func _physics_process(_delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
 	
-	for i: int in _bubble_list.keys():
-		rpc("_sync_position", i, _bubble_list[i].global_position)
+	for i: Node3D in _bubble_list:
+		rpc("_sync_position", i.number, i.global_position)
